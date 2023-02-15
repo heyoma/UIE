06ee3aadfff1b52a378c4301c92cee683efb94ea
heyoma <32898604+heyoma@users.noreply.github.com>
2023-02-15 10:52:55 +0800

    Update sel2record.py
    <unk> span fixing functions using tokenizer

--- scripts/sel2record.py
+++ scripts/sel2record.py
@@ -7,7 +7,7 @@ import json
 
 from uie.sel2record.record import MapConfig
 from uie.sel2record.sel2record import SEL2Record
-
+from transformers import AutoTokenizer
 logger = logging.getLogger(__name__)
 
 
@@ -21,14 +21,20 @@ def main():
     parser.add_argument('-d', dest='decoding', default='spotasoc')
     parser.add_argument('-v', '--verbose', dest='verbose',
                         action='store_true', help='More details information.')
+    parser.add_argument('-m', "--model_path", dest='tokenizer_path', help='The path of tokenizer')
     options = parser.parse_args()
 
     map_config = MapConfig.load_from_yaml(options.map_config)
+
     schema_dict = SEL2Record.load_schema_dict(options.gold_folder)
+    tokenizer = AutoTokenizer.from_pretrained(options.tokenizer_path) if options.tokenizer_path else None
+
+    # tokenizer = None
     sel2record = SEL2Record(
         schema_dict=schema_dict,
         decoding_schema=options.decoding,
         map_config=map_config,
+        tokenizer=tokenizer
     )
 
     data_dict = {
@@ -53,20 +59,20 @@ def main():
             print("gold:", gold_filename) if options.verbose else None
 
             # Only using text and tokens in Gold file
-            gold_list = [json.loads(line) for line in open(gold_filename)]
+            gold_list = [json.loads(line) for line in open(gold_filename, "r", encoding="utf-8")]
             gold_text_list = [gold['text'] for gold in gold_list]
             gold_token_list = [gold['tokens'] for gold in gold_list]
 
-            pred_list = [line.strip() for line in open(pred_filename).readlines()]
+            pred_list = [line.strip() for line in open(pred_filename, "r", encoding="utf-8").readlines()]
 
             assert len(gold_text_list) == len(pred_list)
 
             pred_records = list()
-            for pred, text, tokens in zip(pred_list, gold_text_list, gold_token_list):
+            for i, (pred, text, tokens) in enumerate(zip(pred_list, gold_text_list, gold_token_list)):
                 pred_record = sel2record.sel2record(pred, text, tokens)
                 pred_records += [pred_record]
 
-            with open(os.path.join(pred_folder, record_file), 'w') as output:
+            with open(os.path.join(pred_folder, record_file), 'w', encoding="utf-8") as output:
                 for record in pred_records:
                     output.write(json.dumps(record, ensure_ascii=False) + '\n')
 
